bb77092d6e01c87e870d337a3d810715f6892af1
Alexandra Quispe <alexandra.quispe@vallegrande.edu.pe>
2026-05-21 00:36:39 -0500

    feat(backend): define customer service contract with soft delete capabilities

--- src/main/java/pe/edu/vallegrande/mybackend/service/CustomerService.java
+++ src/main/java/pe/edu/vallegrande/mybackend/service/CustomerService.java
@@ -1,30 +1,18 @@
 package pe.edu.vallegrande.mybackend.service;
 
 import pe.edu.vallegrande.mybackend.model.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Optional;
 
 public interface CustomerService {
-
-   
+    Page<Customer> findAllConFiltros(String razonSocial, String pais, String tipoCliente, Boolean active, Pageable pageable);
     List<Customer> findAll();
-
-    
     List<Customer> findByActive(Boolean active);
-
-  
     Optional<Customer> findById(Long id);
-
-   
     Customer save(Customer customer);
-
-    
     Customer update(Customer customer);
-
-    
     Customer delete(Long id);
-
-  
     Customer restore(Long id);
-    
 }

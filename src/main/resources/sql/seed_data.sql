1462b48a7723d42258e4fc9a8e974e4db86974ec
Anthony Gala <anthony.gala@vallegrande.edu.pe>
2026-06-21 20:30:35 -0500

    refactor(sql): reorganize database scripts into single file

--- src/main/resources/sql/seed_data.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    (SELECT COUNT(*) FROM dbo.customer)               AS clientes,
-    (SELECT COUNT(*) FROM dbo.productos)              AS productos,
-    (SELECT COUNT(*) FROM dbo.customer_orders)        AS pedidos,
-    (SELECT COUNT(*) FROM dbo.customer_order_details) AS detalles;
-GO
